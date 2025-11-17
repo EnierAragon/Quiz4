a9d5b6ba80bc402ee80135897e5c003aeca5d515
EnierAragon <91.enier.91@gmail.com>
2025-11-17 16:04:14 -0600

    Funcionalidad controller

--- src/main/java/cr/ac/ucr/Quiz4_C20504_C31676/controller/MainController.java
+++ src/main/java/cr/ac/ucr/Quiz4_C20504_C31676/controller/MainController.java
@@ -4,6 +4,7 @@
  */
 package cr.ac.ucr.Quiz4_C20504_C31676.controller;
 
+import cr.ac.ucr.Quiz4_C20504_C31676.view.ScreenMain;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,43 +12,53 @@ import java.awt.event.ActionListener;
  *
  * @author enier
  */
-public class MainController implements ActionListener{
+public class MainController implements ActionListener {
+
+    ScreenMain screamCalc;
+
+    public MainController() {
+        initComponets();
+        screamCalc.listen(this);
+    }
+
+    private void initComponets() {
+        screamCalc = new ScreenMain();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case "btnOrigin":
-                
+
                 break;
-                
+
             case "btnScale":
-                
+
                 break;
-                
+
             case "btnSave":
-                
+
                 break;
-                
+
             case "btnDelete":
-                
+
                 break;
-                
+
             case "btnSearch":
-                
+
                 break;
-                
+
             case "btnPresets":
-                
+
                 break;
-                
+
             case "btnDiference":
-                
+
                 break;
-                
-            
+
             default:
                 throw new AssertionError();
         }
     }
-    
+
 }
