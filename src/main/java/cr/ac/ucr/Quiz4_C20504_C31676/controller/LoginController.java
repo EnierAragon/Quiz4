a1acc4c9672c61295ed5d5a37f5dc7efdc9e4a22
Hugo <hjcarranzaarroyo@hotmail.com>
2025-11-17 16:02:52 -0600

    ControllerLogin

---
+++ src/main/java/cr/ac/ucr/Quiz4_C20504_C31676/controller/LoginController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.ucr.Quiz4_C20504_C31676.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author hjcar
+ */
+public class LoginController implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "btnLogin":
+                
+                break;
+                
+            case "btnRegister":
+                
+                break;
+                
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+}
